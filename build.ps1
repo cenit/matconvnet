df3e8db461a2fc4aee2e3322e79c51a3464d2431
Alessandro Fabbri <rommacc@yahoo.it>
2018-03-19 17:00:42 +0100

    wip

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@
 
 mkdir build
 cd build
-#cmake -G "Visual Studio 15 2017 Win64" ..
-cmake -G "Ninja" "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x64-windows" ..
+cmake -G "Visual Studio 15 2017 Win64" "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x64-windows" ..
+#cmake -G "Ninja" "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x64-windows" ..
 cmake --build . --target install #--config Release
 cd ..
