c9a8d393db14aecadbdeb5080baa86459181d3ef
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-03-22 10:46:42 +0100

    linux portable

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
  export CC="/usr/local/bin/gcc-7"
  export CXX="/usr/local/bin/g++-7"
+ BLASLIB="-DMATLAB_BLAS_LIBRARY=/Applications/MATLAB_R2017b.app/bin/maci64/libmwblas.dylib"
 fi
 
 mkdir -p build
 cd build
-cmake .. -DMATLAB_BLAS_LIBRARY=/Applications/MATLAB_R2017b.app/bin/maci64/libmwblas.dylib
+cmake .. ${BLASLIB}
 cmake --build . --target install
 #make VERBOSE=1
 cd ..
