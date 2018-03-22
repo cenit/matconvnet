c3689298b085c69fbfdbd9cbd04102a120eae992
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-03-22 13:46:50 +0100

    works on mac

--- build.sh
+++ build.sh
@@ -4,12 +4,11 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
  export CC="/usr/local/bin/gcc-7"
  export CXX="/usr/local/bin/g++-7"
- BLASLIB="-DMatlab_BLAS_LIBRARY=/Applications/MATLAB_R2017b.app/bin/maci64/libmwblas.dylib"
 fi
 
 mkdir -p build
 cd build
-cmake .. ${BLASLIB}
+cmake ..
 cmake --build . --target install
 #make VERBOSE=1
 cd ..
