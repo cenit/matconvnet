31e0ac7e3ea692d08aedd0ba70d675031549a75e
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-04-15 13:17:10 +0200

    add fixes for mac

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
  export CC="/usr/local/bin/gcc-7"
  export CXX="/usr/local/bin/g++-7"
+ export BLAS_DIR="/usr/local/Cellar/openblas/0.2.20_1"
 fi
 
 mkdir -p build
